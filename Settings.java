ba8102b7cbd5623b1462b154a74eef22b619e5ea
tyfia-rak <134684150+tyfia-rak@users.noreply.github.com>
2023-07-11 01:36:47 -0700

    Add files via upload

---
+++ Settings.java
@@ -0,0 +1,6 @@
+
+public class Settings {
+    public static final String DATABASE_NAME = "user_app";
+    public static final String USER = "postgres";
+    public static final String PASSWORD = "archfiend";
+}
---
+++ Test.java
@@ -0,0 +1,7 @@
+import java.sql.Connection;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(UserCRUD.findUserById(1));
+    }
+}
