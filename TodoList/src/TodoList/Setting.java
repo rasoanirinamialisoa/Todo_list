4e331ee9a06b4b1f7fff213c5558764cecb312c2
Lisa RASOANIRINA <127594297+rasoanirinamialisoa@users.noreply.github.com>
2023-07-13 17:16:40 +0300

    Delete TodoList directory

--- TodoList/src/TodoList/Setting.java
+++
@@ -1,7 +0,0 @@
-package TodoList;
-
-public class Setting {
-    public static final String DATABASE_NAME = "postgres";
-    public static final String USER = "postgres";
-    public static final String PASSWORD = "root";
-}
